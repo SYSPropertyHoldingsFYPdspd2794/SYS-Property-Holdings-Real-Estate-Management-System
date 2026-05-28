143832752a47c570fee759ad2e5d334c23b0b1c0
kahjun <harkahjun@gmail.com>
2026-05-28 23:51:14 +0800

    Update apply_affordable.php

--- customer/apply_affordable.php
+++ customer/apply_affordable.php
@@ -185,7 +185,7 @@ include '../includes/header.php';
                         <a href="track_status.php?success=application_submitted" class="btn btn-outline-dark fw-bold">Track Status</a>
                     <?php else: ?>
                         <a href="#" class="btn btn-danger fw-bold" data-bs-target="#incomeProfileWarningModal" data-bs-toggle="modal" data-bs-dismiss="modal">Update Profile</a>
-                        <button type="button" id="reviewAppBtn" class="btn btn-outline-secondary fw-bold" data-bs-dismiss="modal">Review Application</button>
+                        <a href="track_status.php?tab=housing" class="btn btn-outline-secondary fw-bold">Review Application</a>
                     <?php endif; ?>
                 </div>
             </div>
@@ -217,13 +217,6 @@ document.addEventListener('DOMContentLoaded', function () {
     if (resultModalElement) {
         const resultModal = new bootstrap.Modal(resultModalElement);
         resultModal.show();
-        
-        const reviewAppBtn = document.getElementById('reviewAppBtn');
-        if (reviewAppBtn) {
-            reviewAppBtn.addEventListener('click', function () {
-                resultModal.hide();
-            });
-        }
     }
 });
 </script>
