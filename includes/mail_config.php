d6e77d01e447e41530165f02bd7cd199c5beb37b
kahjun <harkahjun@gmail.com>
2026-06-07 20:52:50 +0800

    email verification

---
+++ includes/mail_config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 587,
+    'smtp_username' => 'harkahjun@gmail.com',
+    'smtp_password' => 'isgf kzqh acrx itiz',
+    'from_email' => 'harkahjun@gmail.com',
+    'from_name' => 'SYS Property Holdings',
+];
+?>
